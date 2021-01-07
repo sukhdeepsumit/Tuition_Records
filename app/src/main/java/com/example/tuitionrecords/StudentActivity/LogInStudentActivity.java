46b95c1b3086b10ef09ba7e31299087257decc59
sukhdeepsumit <sukhi.aditya.sumit@gmail.com>
2021-01-07 16:25:04 +0530

    Signup for student added

--- app/src/main/java/com/example/tuitionrecords/StudentActivity/LogInStudentActivity.java
+++ app/src/main/java/com/example/tuitionrecords/StudentActivity/LogInStudentActivity.java
@@ -2,15 +2,27 @@ package com.example.tuitionrecords.StudentActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.tuitionrecords.R;
 
 public class LogInStudentActivity extends AppCompatActivity {
 
+    TextView signUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_student);
+        signUp=findViewById(R.id.signup);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LogInStudentActivity.this,SignUpStudentActivity.class));
+            }
+        });
     }
 }
