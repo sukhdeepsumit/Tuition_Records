22ab082e9123e6a7e111393613a4e2542d73de41
sukhdeepsumit <sukhi.aditya.sumit@gmail.com>
2021-01-09 15:05:36 +0530

    Navigation Bar added

--- app/src/main/java/com/example/tuitionrecords/TeacherActivity/ShowTeacherActivity.java
+++ app/src/main/java/com/example/tuitionrecords/TeacherActivity/ShowTeacherActivity.java
@@ -1,18 +1,31 @@
 package com.example.tuitionrecords.TeacherActivity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.tuitionrecords.R;
+import com.example.tuitionrecords.StudentActivity.LogInStudentActivity;
+import com.example.tuitionrecords.StudentActivity.ShowStudentActivity;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.Objects;
 
 public class ShowTeacherActivity extends AppCompatActivity {
     ImageView message;
+    NavigationView nav;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,58 @@ public class ShowTeacherActivity extends AppCompatActivity {
         setContentView(R.layout.activity_show_teacher);
 
         message = findViewById(R.id.message_requests);
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        nav=findViewById(R.id.navMenu);
+        drawerLayout=findViewById(R.id.drawer);
+
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId())
+                {
+                    case R.id.myAccount :
+                    {
+                        Toast.makeText(getApplicationContext(), "My Account opened", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    }
+                    case R.id.schedule :
+                    {
+                        Toast.makeText(getApplicationContext(), "Time table opened", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    }
+                    case R.id.contactus :
+                    {
+                        Toast.makeText(getApplicationContext(), "Contact us opened", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    }
+                    case  R.id.howto:
+                    {
+                        Toast.makeText(getApplicationContext(), "How to use opened", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    }
+                    case R.id.logout:
+                    {
+                        Toast.makeText(getApplicationContext(), "Logged out", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(ShowTeacherActivity.this, LogInTeacherActivity.class));
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    }
+                }
+
+                return true;
+            }
+        });
     }
 }
