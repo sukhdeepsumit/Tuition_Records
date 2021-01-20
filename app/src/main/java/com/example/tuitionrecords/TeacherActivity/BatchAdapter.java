eb2311d14f82862bd33a3aea866b79d6c456b092
sukhdeepsumit <sukhi.aditya.sumit@gmail.com>
2021-01-20 23:57:07 +0530

    batches fetching made

---
+++ app/src/main/java/com/example/tuitionrecords/TeacherActivity/BatchAdapter.java
@@ -0,0 +1,65 @@
+package com.example.tuitionrecords.TeacherActivity;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.tuitionrecords.R;
+import com.example.tuitionrecords.ScheduleModel;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
+
+public class BatchAdapter extends FirebaseRecyclerAdapter<ScheduleModel,BatchAdapter.MyViewHolder> {
+
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+
+
+    public BatchAdapter(@NonNull FirebaseRecyclerOptions<ScheduleModel> options) {
+        super(options);
+
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull MyViewHolder holder, int position, @NonNull ScheduleModel model) {
+     holder.subject.setText(model.getSubject());
+     holder.batch.setText(model.getBatch());
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.bacth,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView batch,subject;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            batch=itemView.findViewById(R.id.batchNum);
+            subject=itemView.findViewById(R.id.subjectGet);
+        }
+    }
+}
--- app/src/main/java/com/example/tuitionrecords/TeacherActivity/MyBatches.java
+++ app/src/main/java/com/example/tuitionrecords/TeacherActivity/MyBatches.java
@@ -1,16 +1,55 @@
 package com.example.tuitionrecords.TeacherActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.tuitionrecords.FeeStatusModel;
 import com.example.tuitionrecords.R;
+import com.example.tuitionrecords.ScheduleModel;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MyBatches extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    BatchAdapter batchAdapter;
+    DatabaseReference reference;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_batches);
+        recyclerView=findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        String currentUser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        reference = FirebaseDatabase.getInstance().getReference("Time_Table").child(currentUser);
+
+        FirebaseRecyclerOptions<ScheduleModel> options =
+                new FirebaseRecyclerOptions.Builder<ScheduleModel>()
+                        .setQuery(reference,ScheduleModel.class)
+                        .build();
+
+        batchAdapter=new BatchAdapter(options);
+        recyclerView.setAdapter(batchAdapter);
+
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        batchAdapter.startListening();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        batchAdapter.stopListening();
     }
 }
